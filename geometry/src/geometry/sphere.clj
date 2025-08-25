00d56825c3c47d7f0874d712ad6b2fea3c8a889c
mattosbruno99 <brunomattos255@gmail.com>
2025-08-25 14:36:03 -0300

    feat(geometry): Add sphere.clj

---
+++ geometry/src/geometry/sphere.clj
@@ -0,0 +1,15 @@
+(ns geometry.sphere)
+
+;; Calculate volume of a sphere
+(defn volume-of-sphere
+    "Calculate volume of a sphere based on its radius.
+    Formula: V = 4/3*PI*(radius^3)"
+    [radius]
+    (* (/ 4 3) Math/PI (Math/pow radius 3)))
+
+;; Calculate total surface area of a sphere
+(defn surface-area-of-sphere
+    "Calculate total surface area of a sphere based on its radius.
+    Formula: A = 4*PI*(radius^2)"
+    [radius]
+    (* 4 Math/PI (Math/pow radius 2)))
